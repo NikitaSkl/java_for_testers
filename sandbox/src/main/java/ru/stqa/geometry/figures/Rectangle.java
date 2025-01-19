1926a4e1284f5b261e6a1c00ad6c811f9e845715
Nikita Skliar <nikitassklyar@gmail.com>
2025-01-19 14:29:16 +0300

    Изменено объявление Square с помощью record

--- sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
@@ -1,6 +1,14 @@
 package ru.stqa.geometry.figures;
 
 public class Rectangle {
+    private double a;
+    private double b;
+
+    public Rectangle(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
     public static void printRectangleArea(double side1, double side2) {
         var text=String.format("Площадь прямоугольнка со сторонами %f и %f = %f",side1,side2, rectangleArea(side1, side2));
         System.out.println(text);
