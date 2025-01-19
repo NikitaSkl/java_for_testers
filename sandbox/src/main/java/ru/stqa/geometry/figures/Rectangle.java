1015331220014f8d4da44599a471d3a6b714268a
Nikita Skliar <nikitassklyar@gmail.com>
2025-01-19 13:00:11 +0300

    Выполнена структуризация кода. Исправлены названия методов.

--- sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
@@ -2,10 +2,11 @@ package ru.stqa.geometry.figures;
 
 public class Rectangle {
     public static void printRectangleArea(double side1, double side2) {
-        System.out.println("Площадь прямоугольнка со сторонами "+side1+" и "+side2+" = "+ getRectangleSquare(side1, side2));
+        var text=String.format("Площадь прямоугольнка со сторонами %f и %f = %f",side1,side2, getRectangleArea(side1, side2));
+        System.out.println(text);
     }
 
-    private static double getRectangleSquare(double side1, double side2) {
+    private static double getRectangleArea(double side1, double side2) {
         return side1 * side2;
     }
 }
--- sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
@@ -2,7 +2,8 @@ package ru.stqa.geometry.figures;
 
 public class Square {
     public static void printSquareArea(double side){
-        System.out.println("Площадь квадрата со стороной "+side+" = "+ getSquareArea(side));
+        String text = String.format("Площадь квадрата со стороной %f = %f", side, getSquareArea(side));
+        System.out.println(text);
     }
 
     private static double getSquareArea(double side) {
