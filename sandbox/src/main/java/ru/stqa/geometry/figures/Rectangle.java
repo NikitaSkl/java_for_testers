0699241457c99e9f9908014bd237fab76f473736
Nikita Skliar <nikitassklyar@gmail.com>
2025-01-19 13:37:33 +0300

    Добавлены тесты методов класса Square

--- sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
@@ -2,11 +2,11 @@ package ru.stqa.geometry.figures;
 
 public class Rectangle {
     public static void printRectangleArea(double side1, double side2) {
-        var text=String.format("Площадь прямоугольнка со сторонами %f и %f = %f",side1,side2, getRectangleArea(side1, side2));
+        var text=String.format("Площадь прямоугольнка со сторонами %f и %f = %f",side1,side2, rectangleArea(side1, side2));
         System.out.println(text);
     }
 
-    private static double getRectangleArea(double side1, double side2) {
+    private static double rectangleArea(double side1, double side2) {
         return side1 * side2;
     }
 }
--- sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
@@ -2,11 +2,15 @@ package ru.stqa.geometry.figures;
 
 public class Square {
     public static void printSquareArea(double side){
-        String text = String.format("Площадь квадрата со стороной %f = %f", side, getSquareArea(side));
+        String text = String.format("Площадь квадрата со стороной %f = %f", side, area(side));
         System.out.println(text);
     }
 
-    private static double getSquareArea(double side) {
+    public static double area(double side) {
         return side * side;
+    }
+
+    public static double perimeter(double side) {
+         return 4*side;
     }
 }
