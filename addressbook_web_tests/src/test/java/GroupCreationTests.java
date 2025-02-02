3be6f3555cb2633637b01f9880182fe077272153
Nikita Skliar <nikitassklyar@gmail.com>
2025-02-02 13:57:46 +0300

    Создан общий базовый класс TestBase. Создан модельный класс Group

--- addressbook_web_tests/src/test/java/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/GroupCreationTests.java
@@ -1,68 +1,24 @@
-import org.junit.jupiter.api.BeforeEach;
+import model.Group;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-public class GroupCreationTests {
-    private static WebDriver driver;
-
-    @BeforeEach //фикстура, подготовительный код
-    public void setUp() {
-        if (driver == null) {
-            driver = new ChromeDriver();
-            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
-            driver.get("http://localhost/addressbook/");
-            driver.manage().window().setSize(new Dimension(2064, 1128));
-            driver.findElement(By.name("user")).sendKeys("admin");
-            driver.findElement(By.name("pass")).click();
-            driver.findElement(By.name("pass")).sendKeys("secret");
-            driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
-        }
-    }
+public class GroupCreationTests extends TestBase {
 
     @Test
     public void canCreateGroup() {
-        if (!isElementPresent(By.name("new"))) {
-            driver.findElement(By.linkText("groups")).click();
-        }
-        driver.findElement(By.name("new")).click();
-        driver.findElement(By.name("group_name")).click();
-        driver.findElement(By.name("group_name")).sendKeys("test group name 1");
-        driver.findElement(By.name("group_header")).click();
-        driver.findElement(By.name("group_header")).sendKeys("test group header 1");
-        driver.findElement(By.name("group_footer")).click();
-        driver.findElement(By.name("group_footer")).sendKeys("test group footer 1");
-        driver.findElement(By.name("submit")).click();
-        driver.findElement(By.linkText("group page")).click();
-    }
-
-    private boolean isElementPresent(By locator) {
-        try {
-            driver.findElement(locator);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-
+        openGroupsPage();
+        createGroup(new Group("test group name 1", "test group header 1", "test group footer 1"));
     }
 
     @Test
     public void canCreateGroupWithEmptyName() {
-        if (!isElementPresent(By.name("new"))) {
-            driver.findElement(By.linkText("groups")).click();
-        }
-        driver.findElement(By.name("new")).click();
-        driver.findElement(By.name("group_name")).click();
-        driver.findElement(By.name("group_name")).click();
-        driver.findElement(By.name("group_name")).sendKeys("");
-        driver.findElement(By.name("group_header")).click();
-        driver.findElement(By.name("group_header")).sendKeys("");
-        driver.findElement(By.name("group_footer")).click();
-        driver.findElement(By.name("group_footer")).sendKeys("");
-        driver.findElement(By.name("submit")).click();
-        driver.findElement(By.linkText("group page")).click();
+        openGroupsPage();
+        createGroup(new Group());
+    }
+    @Test
+    public void canCreateGroupWithNameOnly() {
+        openGroupsPage();
+        var emptyGroup=new Group();
+        var groupWithName=emptyGroup.withName("some name");
+        createGroup(groupWithName);
     }
 }
---
+++ addressbook_web_tests/src/test/java/TestBase.java
@@ -0,0 +1,65 @@
+import model.Group;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+//С помощью базового класса избавились от общих элементов в тестах: инициализация переменной, вспомогательные методы
+public class TestBase {
+
+    protected static WebDriver driver;
+
+    protected static void createGroup(Group group) {
+        driver.findElement(By.name("new")).click();
+        driver.findElement(By.name("group_name")).click();
+        driver.findElement(By.name("group_name")).sendKeys(group.name());
+        driver.findElement(By.name("group_header")).click();
+        driver.findElement(By.name("group_header")).sendKeys(group.header());
+        driver.findElement(By.name("group_footer")).click();
+        driver.findElement(By.name("group_footer")).sendKeys(group.footer());
+        driver.findElement(By.name("submit")).click();
+        driver.findElement(By.linkText("group page")).click();
+    }
+
+    protected static void removeGroup() {
+        driver.findElement(By.name("selected[]")).click();
+        driver.findElement(By.name("delete")).click();
+        driver.findElement(By.linkText("group page")).click();
+    }
+
+    @BeforeEach //фикстура, подготовительный код
+    public void setUp() {
+        if (driver == null) {
+            driver = new ChromeDriver();
+            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
+            driver.get("http://localhost/addressbook/");
+            driver.manage().window().setSize(new Dimension(2064, 1128));
+            driver.findElement(By.name("user")).sendKeys("admin");
+            driver.findElement(By.name("pass")).click();
+            driver.findElement(By.name("pass")).sendKeys("secret");
+            driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
+        }
+    }
+
+    protected boolean isElementPresent(By locator) {
+        try {
+            driver.findElement(locator);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+
+    }
+
+    protected void openGroupsPage() {
+        if (!isElementPresent(By.name("new"))) {
+            driver.findElement(By.linkText("groups")).click();
+        }
+    }
+
+    protected boolean isGroupPresent() {
+        return isElementPresent(By.name(("selected[]")));
+    }
+}
