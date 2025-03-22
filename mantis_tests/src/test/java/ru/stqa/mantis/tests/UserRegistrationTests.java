127fd91f361b46b26a6c0249770c0dffb53e6e5b
Nikita Skliar <nikitassklyar@gmail.com>
2025-03-23 00:30:12 +0300

    Добавлен альтернативный тест регистрации юзера через Api

--- mantis_tests/src/test/java/ru/stqa/mantis/tests/UserRegistrationTests.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/tests/UserRegistrationTests.java
@@ -3,18 +3,19 @@ package ru.stqa.mantis.tests;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.stqa.mantis.common.CommonFunctions;
+import ru.stqa.mantis.model.UserData;
 
 import java.time.Duration;
 import java.util.regex.Pattern;
-import java.util.stream.Stream;
+
 public class UserRegistrationTests extends TestBase{
     @Test
     void canRegisterUser(){
         //создать пользователя (адрес) на почтовом сервере (JamesHelper)
         var username=CommonFunctions.randomString(10);
         var email=String.format("%s@localhost", username);
-        //app.jamesCli().addUser(email, "password");
-        app.jamesApi().addUser(email,"password");
+        app.jamesCli().addUser(email, "password");
+        //app.jamesApi().addUser(email,"password");
         //заполняем форму создания и отправляем (браузер)
         app.registration().registerNewUser(email,username);
         //получаем почту (MailHelper)
@@ -35,5 +36,28 @@ public class UserRegistrationTests extends TestBase{
         app.http().login(username,"new_password");
         Assertions.assertTrue(app.http().isLoggedIn());
     }
+    @Test
+    void canRegisterUserAlternative(){
+        var username=CommonFunctions.randomString(10);
+        var email=String.format("%s@localhost", username);
+        var password="password";
+        app.jamesApi().addUser(email,password);
+        app.rest().registerNewUser(new UserData().withUsername(username).withEmail(email));
+        var messages=app.mail().receive(email,password, Duration.ofSeconds(10));
+        var text=messages.get(0).content();
 
+        var pattern= Pattern.compile("http://\\S*");
+        var matcher=pattern.matcher(text);
+        String url = null;
+        if (matcher.find()){
+            url = text.substring(matcher.start(),matcher.end());
+            System.out.println(url);
+        }
+
+        app.registration().confirmRegistration(url);
+        app.registration().updatePassword();
+
+        app.http().login(username,"new_password");
+        Assertions.assertTrue(app.http().isLoggedIn());
+    }
 }
